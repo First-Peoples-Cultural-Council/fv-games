40bfb79020825f906763528397707971afcb9adf
Charlie Bodman <charliebodmangithub@gmail.com>
2017-02-23 20:56:13 -0800

    made some improvements to jigsaw

--- jigsaw/js/config.js
+++ jigsaw/js/config.js
@@ -9,19 +9,69 @@ let defaultGameConfig = {
         preloaderLogo:'assets/images/logo.png',
         backgroundImage:'assets/images/wood.jpg',
         youWin:'assets/images/well-done.png',
-        thumb1:'assets/images/thumb1.png',
-        thumb2:'assets/images/thumb2.png',
-        thumb3:'assets/images/thumb3.png',
-        thumb4:'assets/images/thumb4.png',
         cornerTopLeft:'assets/images/corner1a.png',
         cornerTopRight:'assets/images/corner1b.png',
         cornerBottomLeft:'assets/images/corner1c.png',
         cornerBottomRight:'assets/images/corner1d.png',
-        picture1:'assets/images/picture1.jpg',
-        picture2:'assets/images/picture2.jpg',
-        picture3:'assets/images/picture3.jpg',
-        picture4:'assets/images/picture4.jpg'
-    }
+        arrow:'assets/images/blue_arrow.png'
+    },
+
+    puzzles:[
+        {
+            thumbnail:'assets/images/thumb1.png',
+            picture:'assets/images/picture1.jpg',
+            word:{
+                word:'Bear',
+                english:"Bear"
+            },
+            sound:'assets/sounds/sample.mp3'
+        },
+        {
+            thumbnail:'assets/images/thumb2.png',
+            picture:'assets/images/picture2.jpg',
+            word:{
+                word:'Bear',
+                english:"Bear"
+            },
+            sound:'assets/sounds/sample.mp3'
+        },
+        {
+            thumbnail:'assets/images/thumb3.png',
+            picture:'assets/images/picture3.jpg',
+            word:{
+                word:'Bear',
+                english:"Bear"
+            },
+            sound:'assets/sounds/sample.mp3'
+        },
+        {
+            thumbnail:'assets/images/thumb4.png',
+            picture:'assets/images/picture4.jpg',
+            word:{
+                word:'Bear',
+                english:"Bear"
+            },
+            sound:'assets/sounds/sample.mp3'
+        },
+        {
+            thumbnail:'assets/images/thumb4.png',
+            picture:'assets/images/picture4.jpg',
+            word:{
+                word:'Bear',
+                english:"Bear"
+            },
+            sound:'assets/sounds/sample.mp3'
+        },
+        {
+            thumbnail:'assets/images/thumb4.png',
+            picture:'assets/images/picture4.jpg',
+            word:{
+                word:'Bear',
+                english:"Bear"
+            },
+            sound:'assets/sounds/sample.mp3'
+        }
+    ]
 
 };
 
--- jigsaw/js/gametitle.js
+++ jigsaw/js/gametitle.js
@@ -1,4 +1,14 @@
+import configManager from './config';
+
 class GameTitle {
+
+    init()
+    {
+        this.config = configManager.getConfig();
+        this.page = 0
+        this.perPage = 4;
+        this.pageOffset = 325;
+    }
 
     create()
     {
@@ -9,29 +19,67 @@ class GameTitle {
 
     createThumbnails()
     {
+        const config = this.config;
+        const puzzles = config.puzzles;
 
         this.thumbnails = this.add.group();
         this.thumbnails.inputEnableChildren = true;
-        this.thumbnails.createMultiple(1, ['thumb1', 'thumb2', 'thumb3', 'thumb4'], 0, true);
+        puzzles.forEach((puzzle,index)=>{
+            var style = { font: "bold 30px Arial", fill: "#ffffff", align: "center"};  
+            var text = this.game.add.text(0, 0, "Test\nTest", style);
+
+            var bar = this.game.add.graphics();
+            bar.beginFill(0x000000, 0);
+            bar.drawRect(0, 0, 200, 100);
+    
+            var thumb = this.thumbnails.create(0,0,puzzle.thumbnailKey);
+            text.anchor.set(0.5);
+            text.x = 0;
+            text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+
+            thumb.addChild(bar);
+            thumb.addChild(text);
+
+            thumb.data = { width: 3, height: 3, img: puzzle.pictureKey };
+        }); 
+
+        // this.thumbnails.createMultiple(1, ['thumb1', 'thumb2', 'thumb3', 'thumb4'], 0, true);
         this.thumbnails.setAll('input.useHandCursor', 'input', true);
         this.thumbnails.callAll('anchor.set', 'anchor', 0.5);
-        this.thumbnails.align(2, 2, 400, 300, Phaser.CENTER);
-
+        this.thumbnails.align(Math.round(puzzles.length / 2), 2, 360, 250, Phaser.BOTTOM_RIGHT);
         this.thumbnails.onChildInputOver.add(this.overThumb, this);
         this.thumbnails.onChildInputOut.add(this.outThumb, this);
         this.thumbnails.onChildInputUp.add(this.selectThumb, this);
+        this.thumbnails.x = 40;
+        this.thumbnails.y = 50;
 
-        //  We use the `data` property that Sprites have to store some extra information
-        //  about each jigsaw. The width and height properties are the number of jigsaw
-        //  pieces the puzzle will contain, and the img is the picture key (in the cache)
-        //  that will be cut to create the jigsaw.
-        this.thumbnails.getChildAt(0).data = { width: 3, height: 3, img: 'picture1' };
-        this.thumbnails.getChildAt(1).data = { width: 5, height: 4, img: 'picture2' };
-        this.thumbnails.getChildAt(2).data = { width: 7, height: 6, img: 'picture3' };
-        this.thumbnails.getChildAt(3).data = { width: 10, height: 8, img: 'picture4' };
+
+
+
+            //  One BitmapText per word (so we can change their color when found)
+        var style = { font: "bold 28px Arial", autoUpperCase:true, fill: "#000000" };
+        //  One BitmapText per word (so we can change their color when found)
+        var title = this.add.text(0,0, 'Choose a Puzzle', style);
+        title.anchor.x = 0.5;
+        title.anchor.y = 0.5;
+        title.x = this.game.width / 2;
+        title.y = 25;
+
+        var nextArrow = this.add.image(this.game.width - 15, this.game.height - 15 ,'arrow');
+        nextArrow.anchor.x = 1;
+        nextArrow.anchor.y = 1;
+        nextArrow.inputEnabled = true;
+        nextArrow.events.onInputUp.add(()=>{
+            this.page++;
+            this.add.tween(this.thumbnails).to({x:-(this.page * this.pageOffset)},1500, Phaser.Easing.Elastic.Out, true);
+        })
 
     }
 
+    nextPage()
+    {
+
+    }
     fadeIn()
     {
         var fadeBackground = this.game.add.graphics(0, 0);
