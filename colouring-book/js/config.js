12d33fe8a4a65e4d5b5c0fbbd4a4c969644a2a1a
Charlie Bodman <charliebodmangithub@gmail.com>
2017-03-07 21:17:19 -0800

    fixed colouring book assets and added picture this assets

--- colouring-book/js/config.js
+++ colouring-book/js/config.js
@@ -2,27 +2,27 @@
 let defaultGameConfig = {
 
     libs:{
-        bitmapDataFloodFill:'libs/BitmapDataFloodFill.js',
-        blob:'libs/Blob.js',
-        canvasToBlob:'libs/CanvasToBlob.js',
-        fileSaver:'libs/FileSaver.js'
+        bitmapDataFloodFill:'assets/libs/BitmapDataFloodFill.js',
+        blob:'assets/libs/Blob.js',
+        canvasToBlob:'assets/libs/CanvasToBlob.js',
+        fileSaver:'assets/libs/FileSaver.js'
     },
 
     images:{
-        preloaderLoading:'assets/preloader/loading.png',
-        preloaderLogo:'assets/preloader/logo.png',
-        swatch:'assets/game/swatch.png',
-        selected:'assets/game/selected.png',
-        print:'assets/game/print.png',
-        save:'assets/game/save.png',
-        picture1:'assets/game/picture1.png',
-        picture2:'assets/game/picture2.png',
-        picture3:'assets/game/picture3.png',
-        picture4:'assets/game/picture4.png',
-        thumb1:'assets/game/thumb1.png',
-        thumb2:'assets/game/thumb2.png',
-        thumb3:'assets/game/thumb3.png',
-        thumb4:'assets/game/thumb4.png'
+        preloaderLoading:'assets/images/loading.png',
+        preloaderLogo:'assets/images/logo.png',
+        swatch:'assets/images/swatch.png',
+        selected:'assets/images/selected.png',
+        print:'assets/images/print.png',
+        save:'assets/images/save.png',
+        picture1:'assets/images/picture1.png',
+        picture2:'assets/images/picture2.png',
+        picture3:'assets/images/picture3.png',
+        picture4:'assets/images/picture4.png',
+        thumb1:'assets/images/thumb1.png',
+        thumb2:'assets/images/thumb2.png',
+        thumb3:'assets/images/thumb3.png',
+        thumb4:'assets/images/thumb4.png'
     }
 
 };
