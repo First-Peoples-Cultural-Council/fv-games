4272caa4119af7ae8dff0bdaac41cd68a9d826bf
CharlieBodman <charliebodmangithub@gmail.com>
2018-03-20 20:02:12 -0700

    updated preloader images on all phaser games

--- memory/js/boot.js
+++ memory/js/boot.js
@@ -1,21 +1,24 @@
-import configManager from './config';
+import configManager from '../config';
 
 class Boot {
 
-    init()
-    {
+    init() {
         this.config = configManager.getConfig();
         this.game.stage.backgroundColor = "#FFFFFF";
     }
 
-    preload(){
-        this.game.load.image('loading',  this.config.images.preloaderLoading);
-        this.game.load.image('brand',    this.config.images.preloaderLogo);
+    preload() {
+        this.game.load.image('loading', this.config.images.preloaderLoading);
+        this.game.load.image('brand', this.config.images.preloaderLogo);
     }
 
-    create(){
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    startPreloadScene() {
         this.game.state.start("Preload");
+    }
+
+    create() {
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        setTimeout(this.startPreloadScene.bind(this), 500);
     }
 }
 
