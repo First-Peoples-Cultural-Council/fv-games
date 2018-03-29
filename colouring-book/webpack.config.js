5e644d26df47895a208dba654187034d109c7f2e
CharlieBodman <charliebodmangithub@gmail.com>
2018-03-28 21:54:03 -0700

    upgraded colouring book to webpack 4

--- colouring-book/webpack.config.js
+++ colouring-book/webpack.config.js
@@ -7,53 +7,55 @@ const phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
 const pixi = path.join(phaserModule, 'build/custom/pixi.js')
 const p2 = path.join(phaserModule, 'build/custom/p2.js')
 
+console.log(path.resolve(__dirname, 'entry.js'));
 
 module.exports = {
-    entry:{
-        app:"./entry.js",
-        vendor:['pixi', 'p2', 'phaser']
+    mode: 'development',
+    entry: {
+        app: path.resolve(__dirname, 'entry.js'),
+        vendor: ['pixi', 'p2', 'phaser']
     },
-    output:{
-        path:__dirname + '/www/',
-        filename:"bundle.js"
+    output: {
+        path: __dirname + '/www/',
+        filename: "[name].js"
     },
-    plugins:[
-        new webpack.optimize.CommonsChunkPlugin({  name: 'vendor', minChunks: Infinity, filename: 'vendor.bundle.js'})
-    ],
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:['style-loader', 'css-loader']
+                test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']
             },
             {
-                test:/pixi\.js/,
-                use:{
-                    loader:'expose-loader',
-                    query:'PIXI'
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /pixi\.js/,
+                use: {
+                    loader: 'expose-loader',
+                    query: 'PIXI'
                 }
             },
             {
-                test:/phaser-split\.js$/,
-                use:{
-                    loader:'expose-loader',
-                    query:'Phaser'
+                test: /phaser-split\.js$/,
+                use: {
+                    loader: 'expose-loader',
+                    query: 'Phaser'
                 }
             },
             {
-                test:/p2\.js/,
-                use:{
-                    loader:'expose-loader',
-                    query:'p2'
+                test: /p2\.js/,
+                use: {
+                    loader: 'expose-loader',
+                    query: 'p2'
                 }
             }
         ]
     },
-    resolve:{
-        alias:{
-            phaser:phaser,
-            pixi:pixi,
-            p2:p2
+    resolve: {
+        alias: {
+            phaser: phaser,
+            pixi: pixi,
+            p2: p2
         }
     }
 }
