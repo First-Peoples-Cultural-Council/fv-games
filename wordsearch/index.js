6d1c4aa5ea2772e56fbcac60276fbe847d885006
CharlieBodman <charliebodmangithub@gmail.com>
2018-03-28 22:40:12 -0700

    reverting wordsearch index

--- wordsearch/index.js
+++ wordsearch/index.js
@@ -1,42 +1,40 @@
-/**
- * Scenes
- */
-import Boot       from './js/boot';
-import Preload    from './js/preload';
-import GameTitle  from './js/gametitle';
-import Main       from './js/main';
-import GameOver   from './js/gameover';
+//Styles
+
+//Scenes
+import Boot from './js/boot';
+import Preload from './js/preload';
+import GameTitle from './js/gametitle';
+import Main from './js/main';
+import GameOver from './js/gameover';
 import GameConfig from './js/config';
 
+let game = false;
 
-/**
- * Game manager creates game instance
- */
-class WordSearch
-{
-    constructor(containerElement, config)
-    {
-        GameConfig.setConfig(config);
+export default {
 
-        const game = new Phaser.Game(800, 900, Phaser.AUTO, containerElement);
-        game.state.add("Boot", Boot);
-        game.state.add("Preload", Preload);
-        game.state.add("GameTitle", GameTitle);
-        game.state.add("Main", Main);
-        game.state.add("GameOver", GameOver);
-        game.state.start("Boot");
+    init: (containerElement, config) => {
 
-        this.game = game;
-    }
 
-    destroy()
-    {
-        if(this.game)
-        {
-            this.game.destroy();
-            this.game = null;
+        if (game === false) {
+
+            //Set Game Config
+            GameConfig.setConfig(config);
+
+            //Start Game
+            game = new Phaser.Game(800, 900, Phaser.CANVAS, containerElement);
+            game.state.add("Boot", Boot);
+            game.state.add("Preload", Preload);
+            game.state.add("GameTitle", GameTitle);
+            game.state.add("Main", Main);
+            game.state.add("GameOver", GameOver);
+            game.state.start("Boot");
         }
+    },
+
+    destroy: () => {
+
+        game.destroy();
+        game = false;
+
     }
 }
-
-export default WordSearch;
